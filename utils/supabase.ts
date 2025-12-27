6dc6537c9ea740e1b34e78b8068c884803cf3b47
Simon Manzler <simanzler@gmail.com>
2025-12-27 14:46:45 -0500

    added supabase

---
+++ utils/supabase.ts
@@ -0,0 +1,18 @@
+import { createClient } from "@supabase/supabase-js";
+import "expo-sqlite/localStorage/install";
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl || !supabasePublishableKey) {
+  throw new Error("Supabase URL or publishable key is not set");
+}
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    storage: localStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
