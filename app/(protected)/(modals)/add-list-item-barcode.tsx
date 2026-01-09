abbad13e32a638d4febdeff260a0cecedff5393b
Simon Manzler <simanzler@gmail.com>
2026-01-09 12:55:23 -0700

    updated barcode insert

--- app/(protected)/(modals)/add-list-item-barcode.tsx
+++ app/(protected)/(modals)/add-list-item-barcode.tsx
@@ -1,37 +1,30 @@
-import { useCreateListItem } from "@/api/list-item/mutations";
+import { useAddListItem } from "@/api/list-item/mutations";
 import BarcodeScanner from "@/components/barcode/barcode-scanner";
 import { Tables } from "@/lib/database.types";
 import { useAuthStore } from "@/stores/auth-store";
 import { useHouseholdStore } from "@/stores/household-store";
+import { useToast } from "@/stores/toast-store";
 import { router } from "expo-router";
 import { Alert } from "react-native";
 
 export default function AddListItemBarcode() {
   const { householdId } = useHouseholdStore();
-  const { mutateAsync: createListItem } = useCreateListItem(householdId ?? "");
+  const { mutateAsync: addListItem } = useAddListItem(householdId ?? "");
   const { user } = useAuthStore();
+  const addToast = useToast();
 
   const handleScan = async (data: Tables<"grocery_items">) => {
-    if (!user) {
-      return;
-    }
-
-    console.log({
-      grocery_item_id: data.id,
-      household_id: householdId ?? "",
-      quantity: 1,
-      user_id: user.id,
-    });
+    if (!householdId || !user) return;
 
     try {
-      await createListItem({
-        grocery_item_id: data.id,
-        household_id: householdId ?? "",
+      await addListItem({
+        householdId,
+        groceryItemId: data.id,
         quantity: 1,
-        user_id: user.id,
       });
 
       router.back();
+      addToast("success", "Item added to list");
     } catch (error) {
       Alert.alert("Error", "Failed to add item to list");
       return;
