1b6a40586c1a57c4f3a75b600e51884b9d4c7d3e
Simon Manzler <simanzler@gmail.com>
2026-01-10 07:09:24 -0700

    indicator on init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Toast } from "@/components/toast";
+import { Spinner } from "@/components/ui/spinner";
 import "@/global.css";
 
 import { queryClient } from "@/lib/query-client";
@@ -10,6 +11,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
+import { View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { useUniwind } from "uniwind";
@@ -45,7 +47,12 @@ function RootLayoutNav() {
     initialize();
   }, [initialize]);
 
-  if (initializing) return null;
+  if (initializing)
+    return (
+      <View className="flex-1 items-center justify-center">
+        <Spinner />
+      </View>
+    );
 
   return (
     <Stack screenOptions={{ headerShown: false, animation: "none" }}>
